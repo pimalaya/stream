6449136364c9ef81248e3b4ab6491dd154293481
Clément DOUIN <clement.douin@posteo.net>
2026-05-30 14:36:01 +0200

    bump deps

--- flake.nix
+++ flake.nix
@@ -2,7 +2,10 @@
   description = "Pimalaya streams: TLS/SASL types and blocking I/O sessions";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs = {
+      # until crates.io fix fully backported
+      url = "github:nixos/nixpkgs?tag=25.11&rev=c767db50e209f33ffce3c18165b36101079d367d";
+    };
     fenix = {
       url = "github:nix-community/fenix/monthly";
       inputs.nixpkgs.follows = "nixpkgs";
